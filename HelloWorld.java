4c04f5c3fd918bc27f59d44f81caebc109dc9e7a
Kwame Asante Junior <vie97pie@gmail.com>
2024-01-29 23:31:53 +0000

    enforce Double Quotes for Java String Output

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Single quotes may cause unexpected behavior in Java String output.");
     }
 }
-

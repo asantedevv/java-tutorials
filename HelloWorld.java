c55eb7f70eeef3843de57c49332b2369cfd76d3e
Kwame Asante Junior <vie97pie@gmail.com>
2024-01-29 23:42:01 +0000

    comment in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Single quotes may cause unexpected behavior in Java String output.");
+        // This is a comment
+        System.out.println("How to comment in Java");
     }
 }

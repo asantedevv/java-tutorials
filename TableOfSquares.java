0bd69cc6c6ad18c7c5bde731f78ddaca684e1b95
Kwame Asante Junior <vie97pie@gmail.com>
2024-01-31 16:23:57 +0000

    Create a program that displays the square of numbers from 1 to 10

---
+++ TableOfSquares.java
@@ -0,0 +1,11 @@
+public class TableOfSquares {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number <= 10) {
+            int square = number * number;
+            System.out.println(square);
+            number++;
+        }
+    }
+}
